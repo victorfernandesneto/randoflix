50cfa0c646aac7666228b25d96b2a418d976375c
Victor <vvfernandesneto@gmail.com>
2023-11-22 14:15:26 -0300

    corrigindo bug onde era possível cadastrar dois usuários com o mesmo email

--- tokens/serializers.py
+++ tokens/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- tokens/views.py
+++ tokens/views.py
@@ -1,29 +1,8 @@
-from django.http import JsonResponse
-from rest_framework.views import APIView
+from rest_framework import generics
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from tokens.serializers import UserSerializer
 
 
-class RegisterView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        email = request.data.get('email')
-
-        query1 = User.objects.filter(username=username).first()
-        query2 = User.objects.filter(email=email).first()
-
-        if query1 or query2:
-            return JsonResponse(
-                {'message': 'Username or email already registered'}
-            )
-
-        password = request.data.get('password')
-
-        User.objects.create_user(
-            username=username, email=email, password=password)
-        user = User.objects.filter(username=username).first()
-        return JsonResponse(
-            {'id': user.id,
-             'username': user.username,
-             'email': user.email, }
-        )
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
