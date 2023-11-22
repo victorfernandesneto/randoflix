52d9bab17fff7559fa7f4fc52943ecc99a06218e
Victor <vvfernandesneto@gmail.com>
2023-11-22 13:39:29 -0300

    retirada view de login não utilizada

--- tokens/views.py
+++ tokens/views.py
@@ -5,24 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
 
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        user = authenticate(
-            username=username, password=password)
-
-        refresh = RefreshToken.for_user(user)
-
-        return JsonResponse(
-            {
-                'refresh': str(refresh),
-                'access': str(refresh.access_token)
-            }
-        )
-
-
 class RegisterView(APIView):
     def post(self, request):
         username = request.data.get('username')
